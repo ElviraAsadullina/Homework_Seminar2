5ff8cbc83f17b5989deb63e12f9c7ff4611ccf2f
Elvira <eliasufa@yahoo.com>
2022-08-23 01:14:02 +0500

    Решена первая задача

---
+++ Task_1/Program.cs
@@ -0,0 +1,5 @@
+﻿int num = new Random().Next(100, 1000);
+Console.WriteLine($"Случайно сгенерированное число: {num}");
+int dozens_count = num / 10;
+int digit_middle = dozens_count % 10;
+Console.WriteLine ($"Вторая цифра данного числа: {digit_middle}");
